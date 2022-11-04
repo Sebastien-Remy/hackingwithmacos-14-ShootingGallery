05e0466a9fb4b16c537af4daba0b403937e30de3
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-04 16:23:06 +0100

    initial commit

---
+++ ShootingGallery/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  ShootingGalley
+//
+//  Created by Sebastien REMY on 04/11/2022.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+    }
+}
