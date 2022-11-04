41e8ae05422a4c047e4ca56fb17536868798aca3
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-04 16:16:05 +0100

    Initial Commit

---
+++ ShootingGalley/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  ShootingGalley
+//
+//  Created by Sebastien REMY on 04/11/2022.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
